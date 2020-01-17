1b9746873b181514efb88abf5f85b0878eb62c02
longnh18 <v.longnh18@vinid.net>
2020-01-17 16:00:41 +0700

    update code

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require('path');
 const port = 3000
 // Serve static files....
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/src'));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/pages/home.html'));
+    res.sendFile(path.join(__dirname + '/src/pages/home.html'));
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
